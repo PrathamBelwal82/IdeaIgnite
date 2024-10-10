82a5caae700b1f9ef67b5d56cf80105ea5b74bd9
PrathamBelwal82 <prathambelwal82@gmail.com>
2024-10-10 16:19:43 +0530

    work on QA

--- client/components/AllEvents.jsx
+++ client/components/AllEvents.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { useParams, Link } from 'react-router-dom';
-import "../src/AllEvents.css"; // Make sure to include your CSS styles
+
 
 const AllEvents = () => {
   const { category } = useParams(); // Get category from URL
